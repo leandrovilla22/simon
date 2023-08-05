95f8286bf8211a3b64124f3fd27bd60856844aef
Leandro Ezequiel Villa <lvilla@avalian.com>
2023-08-05 17:59:25 -0300

    Puntaje, Cronometro, Responsive, Form sin validar

--- script.js
+++ script.js
@@ -2,35 +2,39 @@
 var sequence = [];
 var playerSequence = [];
 var level = null;
+var point = 0;
 var buttonsDisabled = true;
 var strictMode = false;
 var buttonElements =null; //document.querySelectorAll('.simon-btn');
-var startButton = document.getElementById('start-btn');
-
-// Start button event listener
-//startButton.addEventListener('click', startGame);
+//var startButton = document.getElementById('start-btn');
+const timer = document.getElementsByClassName('timer');
+const watch = document.getElementById('watch');
+let timerInterval;
+let runingTime= 0;
 
 // Button click event listeners
 function clicksecuencia(){
 for (let h = 0; h < buttonElements.length; h++) {
   buttonElements[h].addEventListener('click', handleButtonClick);
+  
 }
 }
-function nameJugador(){
-  let nameJugador = document.getElementById('nameJugador').value
-  console.log(nameJugador)
-  if (nameJugador===''){
+function namePlayer(){
+  let namePlayer1 = document.getElementById('namePlayer').value
+  console.log(namePlayer1)
+  if (namePlayer1===''){
     alert('Ingrese su nombre')
   }
   else{
     startGame();
+    startTimer(); // Iniciar el cronómetro al comenzar el juego    
   }
 }
 
 
 // Start the game
 function startGame() {
-  //console.log('estoy aca')
+  console.log('estoy aca')
   buttonElements = document.querySelectorAll('.simon-btn');
   clicksecuencia();
   sequence = [];
@@ -42,26 +46,22 @@ function startGame() {
 
 // Generate the sequence
 function generateSequence() {
- 
-  //for (let j = 0; j < level; j++) {
-
-    var randomNum = Math.floor(Math.random() * 4);
-    sequence.push(randomNum);
-    //cambiaColor();
-//  }
+  var randomNum = Math.floor(Math.random() * 4);
+  sequence.push(randomNum);
   console.log('level' , level, 'sequence', sequence);
 }
 
 // Play the sequence
 function playSequence() {
-  var i = 0;
+  let i = 0;
+  
   var interval = setInterval(function() {
     playButton(sequence[i]);
     i++;
     if (i >= sequence.length) {
       clearInterval(interval);
       buttonsDisabled = false;
-    }
+      }
   }, 1000);
 }
 
@@ -69,65 +69,13 @@ function playSequence() {
 function handleButtonClick(event) {
   if (!buttonsDisabled) {
     var buttonId = event.target.id;
+    console.log('EL EVENT TARGET ID ES',event.target.id);
     var buttonIndex = getButtonIndex(buttonId);
     playButton(buttonIndex);
     playerSequence.push(buttonIndex);
     checkPlayerSequence();
   }
 }
-
-// Play a button
-function playButton(buttonIndex) {
-  //console.log('playbutton', buttonElements);
-  var buttonElement = buttonElements[buttonIndex];
-  buttonElement.classList.add('active');
-  setTimeout(function() {
-    buttonElement.classList.remove('active');
-  }, 500);
-}
-
-// Check player's sequence
-function checkPlayerSequence() {
-  console.log('checkPlayerSequence')
-  if (playerSequence.length === sequence.length) {
-    if (arraysMatch(playerSequence, sequence)) {
-      playerSequence = [];
-      level++;
-      console.log('Siguiente Nivel')
-      generateSequence();
-    
-      setTimeout(function() {
-        console.log('comienza el nivel' , level)
-        playSequence();
-      }, 1000);
-    } else {
-      if (strictMode) {
-        alert('Game Over!');
-        startGame();
-      } else {
-        alert('Try Again!');
-        playerSequence = [];
-        setTimeout(function() {
-          playSequence();
-        }, 1000);
-      }
-    }
-  }
-}
-
-// Check if two arrays match
-function arraysMatch(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
 // Get button index
 function getButtonIndex(buttonId) {
   switch (buttonId) {
@@ -141,3 +89,98 @@ function getButtonIndex(buttonId) {
       return 3;
   }
 }
+// Play a button
+function playButton(buttonIndex) {
+  //console.log('playbutton', buttonElements);
+  var buttonElement = buttonElements[buttonIndex];
+  buttonElement.classList.add('active');
+  setTimeout(function() {
+    buttonElement.classList.remove('active');
+  }, 200);
+}
+
+// Check player's sequence
+function checkPlayerSequence() {
+  console.log('checkPlayerSequence', playerSequence)
+  
+  if (playerSequence.length === sequence.length) {
+    
+    if (arraysMatch(playerSequence, sequence)) {
+      playerSequence = [];
+      jugador = document.getElementById('namePlayer').value
+      level++;
+      console.log('Siguiente Nivel')
+      generateSequence();
+        setTimeout(function() {
+        console.log('comienza el nivel' , level)
+        playSequence();
+      }, 1000);
+    } else {
+      if (strictMode) {
+        alert('Game Over!');
+        startGame();
+      } else {
+        //console.log(point);
+        //stopTimer();
+        //alert('Reintentalo '+ jugador + ' estas en el Nivel'+ level +' tu puntuacion actual es:' + point );
+        const continueGame = confirm('¡Incorrecto! ¿Deseas continuar?');
+        if (continueGame) {
+          playerSequence = [];
+        setTimeout(function() {
+        playSequence();
+        }, 1000);
+        } else {
+          stopTimer();
+          alert('Reintentalo '+ jugador + ' estas en el Nivel'+ level +' tu puntuacion actual es:' + point );
+          
+        }
+      }
+    }
+  }
+}
+
+  
+// Check if two arrays match
+function arraysMatch(arr1, arr2) {
+  
+  if (arr1.length !== arr2.length) {
+       return false;
+    
+  }
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+      }
+      else{
+        point ++;
+        console.log('Tu puntuacion es:', point);
+      }
+  }
+  return true;
+}
+
+
+
+// Función para iniciar el cronómetro
+function startTimer() {
+  timerInterval = setInterval(() => {
+    runingTime += 10; // Incremento el tiempo en 10 milisegundos (0.01 segundos)
+    calculateTimer();
+  }, 10); // Ejecutar cada 10 milisegundos (0.01 segundos)
+}
+
+// Función para detener el cronómetro
+function stopTimer() {
+  clearInterval(timerInterval);
+  runingTime = 0;
+  calculateTimer();
+}
+
+// Actualizar el cronómetro en el reloj
+function calculateTimer() {
+  const totalSeconds = Math.floor(runingTime / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  watch.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
