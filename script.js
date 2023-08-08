322233aaf830b11e5bf56ce528c17744cd1504b6
Leandro Ezequiel Villa <lvilla@avalian.com>
2023-08-08 12:02:54 -0300

    Correciones de Camel Case

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var level = null;
 var point = 0;
 var buttonsDisabled = true;
 var strictMode = false;
-var buttonElements =null; //document.querySelectorAll('.simon-btn');
-//var startButton = document.getElementById('start-btn');
+var buttonElements =null;
+//var startButton = document.getElementById('startBtn');
 const timer = document.getElementsByClassName('timer');
 const watch = document.getElementById('watch');
 let timerInterval;
@@ -14,9 +14,8 @@ let runingTime= 0;
 
 // Button click event listeners
 function clicksecuencia(){
-for (let h = 0; h < buttonElements.length; h++) {
-  buttonElements[h].addEventListener('click', handleButtonClick);
-  
+for (let h = 0; h < buttonElements.length; h++){
+  buttonElements[h].addEventListener('click', handleButtonClick);  
 }
 }
 function namePlayer(){
@@ -30,12 +29,10 @@ function namePlayer(){
     startTimer(); // Iniciar el cronómetro al comenzar el juego    
   }
 }
-
-
 // Start the game
-function startGame() {
+function startGame(){
   console.log('estoy aca')
-  buttonElements = document.querySelectorAll('.simon-btn');
+  buttonElements = document.querySelectorAll('.simonBtn');
   clicksecuencia();
   sequence = [];
   level = 1;
@@ -43,31 +40,27 @@ function startGame() {
   playSequence();
   buttonsDisabled = false;
 }
-
 // Generate the sequence
-function generateSequence() {
+function generateSequence(){
   var randomNum = Math.floor(Math.random() * 4);
   sequence.push(randomNum);
   console.log('level' , level, 'sequence', sequence);
 }
-
 // Play the sequence
-function playSequence() {
-  let i = 0;
-  
-  var interval = setInterval(function() {
+function playSequence(){
+  let i = 0; 
+  var interval = setInterval(function(){
     playButton(sequence[i]);
     i++;
-    if (i >= sequence.length) {
+    if (i >= sequence.length){
       clearInterval(interval);
       buttonsDisabled = false;
       }
   }, 1000);
 }
-
 // Handle button click
-function handleButtonClick(event) {
-  if (!buttonsDisabled) {
+function handleButtonClick(event){
+  if (!buttonsDisabled){
     var buttonId = event.target.id;
     console.log('EL EVENT TARGET ID ES',event.target.id);
     var buttonIndex = getButtonIndex(buttonId);
@@ -77,8 +70,8 @@ function handleButtonClick(event) {
   }
 }
 // Get button index
-function getButtonIndex(buttonId) {
-  switch (buttonId) {
+function getButtonIndex(buttonId){
+  switch (buttonId){
     case 'green':
       return 0;
     case 'red':
@@ -90,33 +83,32 @@ function getButtonIndex(buttonId) {
   }
 }
 // Play a button
-function playButton(buttonIndex) {
+function playButton(buttonIndex){
   //console.log('playbutton', buttonElements);
   var buttonElement = buttonElements[buttonIndex];
   buttonElement.classList.add('active');
-  setTimeout(function() {
+  setTimeout(function(){
     buttonElement.classList.remove('active');
   }, 200);
 }
-
 // Check player's sequence
-function checkPlayerSequence() {
+function checkPlayerSequence(){
   console.log('checkPlayerSequence', playerSequence)
   
-  if (playerSequence.length === sequence.length) {
+  if (playerSequence.length === sequence.length){
     
-    if (arraysMatch(playerSequence, sequence)) {
+    if (arraysMatch(playerSequence, sequence)){
       playerSequence = [];
       jugador = document.getElementById('namePlayer').value
       level++;
       console.log('Siguiente Nivel')
       generateSequence();
-        setTimeout(function() {
+        setTimeout(function(){
         console.log('comienza el nivel' , level)
         playSequence();
       }, 1000);
     } else {
-      if (strictMode) {
+      if (strictMode){
         alert('Game Over!');
         startGame();
       } else {
@@ -124,9 +116,9 @@ function checkPlayerSequence() {
         //stopTimer();
         //alert('Reintentalo '+ jugador + ' estas en el Nivel'+ level +' tu puntuacion actual es:' + point );
         const continueGame = confirm('¡Incorrecto! ¿Deseas continuar?');
-        if (continueGame) {
+        if (continueGame){
           playerSequence = [];
-        setTimeout(function() {
+        setTimeout(function(){
         playSequence();
         }, 1000);
         } else {
@@ -137,18 +129,14 @@ function checkPlayerSequence() {
       }
     }
   }
-}
-
-  
+}  
 // Check if two arrays match
-function arraysMatch(arr1, arr2) {
-  
-  if (arr1.length !== arr2.length) {
-       return false;
-    
+function arraysMatch(arr1, arr2){ 
+  if (arr1.length !== arr2.length){
+       return false;    
   }
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+  for (var i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]){
       return false;
       }
       else{
@@ -158,37 +146,31 @@ function arraysMatch(arr1, arr2) {
   }
   return true;
 }
-
-
-
 // Función para iniciar el cronómetro
-function startTimer() {
+function startTimer(){
   timerInterval = setInterval(() => {
     runingTime += 10; // Incremento el tiempo en 10 milisegundos (0.01 segundos)
     calculateTimer();
   }, 10); // Ejecutar cada 10 milisegundos (0.01 segundos)
 }
-
 // Función para detener el cronómetro
-function stopTimer() {
+function stopTimer(){
   clearInterval(timerInterval);
   runingTime = 0;
   calculateTimer();
 }
-
 // Actualizar el cronómetro en el reloj
-function calculateTimer() {
+function calculateTimer(){
   const totalSeconds = Math.floor(runingTime / 1000);
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
   watch.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
-
 //Funciones para el Formulario
-function validarFormulario() {
-  var nombre = document.getElementById('nombre').value;
-  var email = document.getElementById('correo').value;
-  var comentario = document.getElementById('textarea').value;
+function validateForm(){
+  var nombre = document.getElementById('nameForm').value;
+  var email = document.getElementById('validateEmail').value;
+  var comentario = document.getElementById('textArea').value;
 
   var nombreValido = /^[A-Za-z\s]+$/.test(nombre);
   var emailValido = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email);
@@ -197,10 +179,10 @@ function validarFormulario() {
   var asunto = "Consulta desde el formulario de la página";
   var mailtoLink = "mailto:" + destinatario + "?subject=" + encodeURIComponent(asunto) + "&body=" + encodeURIComponent(comentario);
 
-  if (!nombreValido) {
+  if (!nombreValido){
     alert('Ingrese un nombre válido (solo letras y espacios)');
     return false;
-} else if (!emailValido) {
+} else if (!emailValido){
     alert('Ingrese un email válido');
     return false;
 } else {
